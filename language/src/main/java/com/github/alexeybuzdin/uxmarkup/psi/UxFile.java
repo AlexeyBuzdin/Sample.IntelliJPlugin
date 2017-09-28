2e605cae8f4d47578a122c444f8cf00a5b6e3d16
Alexey Buzdin <alex.buzdin@gmail.com>
2017-09-28 12:54:38 +0300

    Added BNF

---
+++ language/src/main/java/com/github/alexeybuzdin/uxmarkup/psi/UxFile.java
@@ -0,0 +1,32 @@
+package com.github.alexeybuzdin.uxmarkup.psi;
+
+import com.github.alexeybuzdin.uxmarkup.UxFileType;
+import com.github.alexeybuzdin.uxmarkup.UxLanguage;
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class UxFile extends PsiFileBase {
+    public UxFile(@NotNull FileViewProvider viewProvider) {
+        super(viewProvider, UxLanguage.getInstance());
+    }
+
+    @NotNull
+    @Override
+    public FileType getFileType() {
+        return UxFileType.INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return "Ux File";
+    }
+
+    @Override
+    public Icon getIcon(int flags) {
+        return super.getIcon(flags);
+    }
+}
